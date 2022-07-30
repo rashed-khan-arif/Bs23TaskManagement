4164361d4072720ae6436c55694ab5a1f8365d35
Rashed Khan Arif <arifkhan0713@gmail.com>
2022-07-31 04:06:58 +0600

    finalize

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Container } from 'react-bootstrap';
+
+
+
 export default function Login() {
     const navigate = useNavigate()
     return (
-        <div>
-            <h1>Login Page</h1>
-            <Button
-                onClick={(e) => {
-                    navigate("/", { replace: true });
-                }}
-            >
-                Login
-            </Button>
-        </div >
+        <Container>
+            <Form style={{ marginTop: "50px" }}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button
+                    onClick={(e) => {
+                        navigate("/tasks", { replace: true });
+                    }}
+                    variant="primary" type="button">
+                    Login
+                </Button>
+            </Form>
+        </Container>
+
     );
 }
